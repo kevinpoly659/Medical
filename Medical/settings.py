c2c640f9d88a6e67c25fb5167dc52edfd59ba177
kevinpoly659 <112921938+kevinpoly659@users.noreply.github.com>
2023-04-23 21:33:18 +0530

    Update settings.py

--- Medical/settings.py
+++ Medical/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
-        'PASSWORD':config('9895'),
+        'PASSWORD':config('DB_PASSWORD'),
         'HOST': config('DB_HOST')
     }
 }
